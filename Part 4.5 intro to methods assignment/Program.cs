62ea00badeade8b500d006e9807997ba412e2ffb
Asher Heywood Peters <339513541@GoTVDSB.ca>
2023-09-22 11:02:25 -0400

    Bat & frog.

--- Part 4.5 intro to methods assignment/Program.cs
+++ Part 4.5 intro to methods assignment/Program.cs
@@ -10,7 +10,26 @@ namespace Part_4._5_intro_to_methods_assignment
     {
         static void Main(string[] args)
         {
-
+            Bat();
+            Frog();
+        }
+        public static void Bat()
+        {
+            Console.WriteLine("        _   ,_,   _");
+            Console.WriteLine("       / `'=) (='` \\");
+            Console.WriteLine("      /.-.-.\\ /.-.-.\\ ");
+            Console.WriteLine("      `      \"      `");
+        }
+        public static void Frog()
+        {
+            Console.WriteLine("              _");
+            Console.WriteLine("  __   ___.--'_`.");
+            Console.WriteLine(" ( _`.'. -   'o` )");
+            Console.WriteLine(" _\\.'_'      _.-'");
+            Console.WriteLine("( \\`. )    //\\`");
+            Console.WriteLine(" \\_`-'`---'\\\\__,");
+            Console.WriteLine("  \\`        `-\\");
+            Console.WriteLine("   `");
         }
     }
 }
